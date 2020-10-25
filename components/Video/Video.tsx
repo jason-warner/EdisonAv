163c241d5890be679217fcfd1f77a663d1d07c0f
thuperman <thuperman@protonmail.com>
2020-10-25 00:34:22 -0400

    YouTube autoplay ios fix attempt: 12

--- components/Video/Video.tsx
+++ components/Video/Video.tsx
@@ -5,14 +5,13 @@ import YouTube from 'react-youtube';
 
 // splashButton,
 const Video = ({ iosDevice }) => {
-    // console.log("sb " + splashButton)
     let device = iosDevice;
     const
         [button, setButton] = useState(null),
         buttonRef = useRef(null),
         opts = {
             playerVars: {
-                // autoplay: 1 as 1,
+                autoplay: 1 as 1,
                 mute: 1 as 1,
                 controls: 0 as 0,
                 enablejsapi: 1 as 1,
@@ -22,48 +21,33 @@ const Video = ({ iosDevice }) => {
 
     const onReady = (Event: { target: any }) => {
         // access to player in all event handlers via event.target
-        // Event.target.pauseVideo();
         playVid(Event);
     }
     const playVid = (Event: { target: any }) => {
         const button = buttonRef.current;
         setButton(button);
-        button.onclick = () => Event.target.playVideo()
-    };
-    // device && button.onclick(() => {
-    //     Event.target.playVideo();
-    // });
-    // access to player in all event handlers via event.target
+        device && button.onclick(() => Event.target.playVideo());
+    }
 
-console.log(device);
-
-// const onReady = () => {
-//     playVid;
-// }
-// const playVid = (Event: { target: any }) => {
-//     const button = buttonRef.current;
-//     setButton(button);
-//     button.addEventListener("click", () => {
-//         console.log(Event.target)
-//         Event.target.pauseVideo();
-//     });
-// }
+    console.log(device);
 
 
-return (
-    <main className={styles.vidContainer}>
-        <button ref={buttonRef} className={styles.playButton} onClick={playVid} >
-            Play Video
-            </button>
-        <YouTube
-            className={styles.format}
-            videoId={"N31pvPzqJAY"}
-            opts={opts}
-            onReady={onReady}
-        />
+    return (
+        <main className={styles.vidContainer}>
+            { device &&
+                <button ref={buttonRef} className={styles.playButton} onClick={playVid} >
+                    Play Video
+                </button>
+            }
+            <YouTube
+                className={styles.format}
+                videoId={"N31pvPzqJAY"}
+                opts={opts}
+                onReady={onReady}
+            />
 
-    </main>
-)
+        </main>
+    )
 }
 
 export default Video;
