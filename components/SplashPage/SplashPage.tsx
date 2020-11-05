c1f12648cfb63dee2e0fe0932d7f9359a3db4d4a
thuperman <thuperman@protonmail.com>
2020-11-05 00:22:17 -0500

    ios sync attempt 4

--- components/SplashPage/SplashPage.tsx
+++ components/SplashPage/SplashPage.tsx
@@ -65,7 +65,7 @@ const SplashPage = () => {
         iosAudio = new Audio(iosSong.src);
       setAudio(iosAudio);
       // !iosDevice && videoReady ? audio.play() : undefined;
-      return getVid && context.resume() && iosAudio.play();
+      return getVid ? context.resume() && audio.play() : undefined;
     }
   }
   // videoReady && alert("video ready");
