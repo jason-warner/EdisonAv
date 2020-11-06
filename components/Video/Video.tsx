bedfb745ecc91f20d97bdafa2c9bb106220a78ae
thuperman <thuperman@protonmail.com>
2020-11-05 22:50:29 -0500

    measly ios audio/video sync attempt: 5

--- components/Video/Video.tsx
+++ components/Video/Video.tsx
@@ -14,7 +14,6 @@ const Video = ({ setVideoReady, setdaVid, playVid }) => {
     const onReady = (Event: { target: any }) => {
         setVideoReady(true);
         playVid(Event);
-        Event.target.playVideo();
     }
 
     const onPlay = () => {
