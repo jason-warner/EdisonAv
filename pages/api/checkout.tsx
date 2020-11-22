9ce8865a546448a55bbcabdebc5df524dca266e6
thuperman <thuperman@protonmail.com>
2020-11-22 13:17:49 -0500

    adding ecommerce to merch page 2

--- pages/api/checkout.tsx
+++ pages/api/checkout.tsx
@@ -3,14 +3,14 @@
 // const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
 
 
-// export default () => {
-//     return (
-//         <div>
-//             <h1>Checkout</h1>
+export default () => {
+    return (
+        <div>
+            <h1>Checkout</h1>
 
-//             <button role={link} onClick={}>
+            {/* <button role={link} onClick={}>
 
-//             </button>
-//         </div>
-//     )
-// }
+            </button> */}
+        </div>
+    )
+}
